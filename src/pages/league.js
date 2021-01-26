57d8b20b57a29bbc668baaed5dcd37568b3f99f2
Christopher Swinton <33087553+Swinny96@users.noreply.github.com>
2021-01-26 22:25:11 +0000

    Fix to Build

--- src/pages/league.js
+++ src/pages/league.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout/layout"
+import LeagueTable from "../components/layout/league/leaguetable"
 import SEO from "../components/layout/seo"
-import LeagueTable from "../components/league/leaguetable"
 
 const LeaguePage = () => (
   <Layout>
