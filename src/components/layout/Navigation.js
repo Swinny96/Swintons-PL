98aaa1aee7497607261532d3a9eb7b0b4736f1c9
Christopher Swinton <33087553+Swinny96@users.noreply.github.com>
2021-01-26 21:55:20 +0000

    Test 2 for Styled-Components

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,26 +1,89 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 import logo from "../../../static/images/logos/logo.png"
-import "./layout.css"
 
 const Navigation = () => (
-  <header>
+  <MyHeader>
     <Link to="/">
-      <div className="Logo-Wrapper">
-        <img className="Logo" src={logo} />
-        <div className="Logo-Text">
-          <span className="Logo-Span">The Swintons</span>
-          <h1 className="Logo-Heading">Predictions League</h1>
-        </div>
-      </div>
+      <LogoWrapper className="Logo-Wrapper">
+        <Logo className="Logo" src={logo} />
+        <LogoText className="">
+          <LogoSpan>The Swintons</LogoSpan>
+          <LogoHeading>Predictions League</LogoHeading>
+        </LogoText>
+      </LogoWrapper>
     </Link>
-    <div className="Nav">
-      <a className="Nav-Link" href="../predictions">Predictions</a>
-      <a className="Nav-Link" href="../league">League standings</a>
-      <a className="Nav-Link" href="../table">PL Table</a>
-      <a className="Nav-Link" href="../fixtures">PL Fixtures</a>
-    </div>
-  </header>
+    <HeaderLinks>
+      <Nav href="../predictions">Predictions</Nav>
+      <Nav href="../league">League standings</Nav>
+      <Nav href="../table">PL Table</Nav>
+      <Nav href="../fixtures">PL Fixtures</Nav>
+    </HeaderLinks>
+  </MyHeader>
 )
 
 export default Navigation
+
+const MyHeader = styled.header``
+
+const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+const LogoText = styled.div`
+  margin-left: 12px;
+  height: 100%;
+  font-style: normal;
+  text-align: left;
+`
+const LogoSpan = styled.span`
+  font-size: 10px;
+  line-height: 12px;
+  letter-spacing: 0.09em;
+  text-transform: uppercase;
+`
+const LogoHeading = styled.h1`
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 23px;
+  margin: 0;
+`
+const Logo = styled.img`
+  max-width: 44px;
+`
+
+const HeaderLinks = styled.div`
+  overflow: auto;
+  white-space: nowrap;
+  padding: 0px;
+  margin-top: 25px;
+`
+
+const Nav = styled.a`
+  display: inline-block;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  line-height: 16px;
+  letter-spacing: 0em;
+  margin-right: 24px;
+  padding-bottom: 12px;
+  transition: 0.3s;
+  text-decoration: none;
+  color: var(--EPL-White);
+
+  :active {
+    padding-bottom: 8px;
+    border-bottom: 4px solid var(--EPL-Pink);
+  }
+
+  :hover {
+    padding-bottom: 8px;
+    border-bottom: 4px solid var(--EPL-Pink);
+  }
+`
