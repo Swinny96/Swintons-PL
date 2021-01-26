1d1cd3bf8602857f4d91c2dce71f6a8a0d07ab94
Christopher Swinton <33087553+Swinny96@users.noreply.github.com>
2021-01-26 19:57:27 +0000

    Quick Template Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
+import styled from "styled-components"
 
 function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Container>
+        <img src="/images/logos/logo.svg" alt="logo" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+      </Container>
     </Layout>
   )
 }
 
 export default IndexPage
+
+const Container = styled.div`
+  margin: 16px;
+`
